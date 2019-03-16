05ed83a3065ad24b2b8a5d00c20c76aebfe6e19c
cech.castellon <47920140+cechgamer@users.noreply.github.com>
2019-03-16 10:51:52 -0400

    Primer Commit

---
+++ productoBackend/productoBackend/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace productoBackend.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<productoBackend.Models.Producto> Productoes { get; set; }
+    }
+}
---
+++ productoBackend/productoBackend/Models/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace productoBackend.Models
+{
+    public class Producto
+    {
+
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre del Producto")]
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+
+
+    }
+}
